2149df275ea523204a393b91e24407b3e8f55b2c
Giang Nguyen <hoanggia@uni-bremen.de>
2022-06-15 00:18:09 +0200

    Fix bug ModuleNotFoundError

--- scripts/rosprolog_rest.py
+++ scripts/rosprolog_rest.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 import rospy
 import os
 from gevent.pywsgi import WSGIServer  # Web Server
@@ -6,7 +7,70 @@ from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 from werkzeug.exceptions import BadRequest
 from flask_restplus import Api, Resource, fields
-from RosprologRestClient import RosprologRestClient
+from json_prolog_msgs.srv import PrologQuery, PrologNextSolution, PrologNextSolutionResponse, PrologFinish
+import json
+
+
+class RosprologRestClient:
+    def __init__(self, name_space='rosprolog', timeout=None, wait_for_services=True):
+        """
+        @type 	name_space: str
+        @param 	timeout: Amount of time in seconds spend waiting for rosprolog to become available.
+        @type 	timeout: int
+        """
+        self.id = 0
+        self._simple_query_srv = rospy.ServiceProxy(
+            '{}/query'.format(name_space), PrologQuery)
+        self._next_solution_srv = rospy.ServiceProxy(
+            '{}/next_solution'.format(name_space), PrologNextSolution)
+        self._finish_query_srv = rospy.ServiceProxy(
+            '{}/finish'.format(name_space), PrologFinish)
+        if wait_for_services:
+            rospy.loginfo('waiting for {} services'.format(name_space))
+            self._finish_query_srv.wait_for_service(timeout=timeout)
+            self._simple_query_srv.wait_for_service(timeout=timeout)
+            self._next_solution_srv.wait_for_service(timeout=timeout)
+            rospy.loginfo('{} services ready'.format(name_space))
+
+    def post_query(self, query):
+        """
+        @param	query: the query string
+        @type		query: str
+        """
+        self.id += 1
+        result = self._simple_query_srv(id=str(self.id), query=query)
+        if result.ok:
+            return True
+        else:
+            self.id -= 1
+            return False
+
+    def get_solutions(self, max_solution_count):
+        """
+        @param	solution_count: the query string
+        @type		solution_count: int
+        """
+        solutions = []
+        if max_solution_count > 0:
+            try:
+                while(len(solutions) < max_solution_count):
+                    next_solution = self._next_solution_srv(id=str(self.id))
+                    if next_solution.status == PrologNextSolutionResponse.OK:
+                        solutions.append(
+                            dict(json.loads(next_solution.solution)))
+                    elif next_solution.status == PrologNextSolutionResponse.NO_SOLUTION:
+                        if not solutions:
+                            raise BadRequest('No solution found')
+                        break
+                    else:
+                        raise BadRequest('Bad query')
+            finally:
+                self.finish_query()
+        return solutions
+
+    def finish_query(self):
+        self._finish_query_srv(id=str(self.id))
+
 
 # Set KnowRob version and KnowRob Port from environment variables
 KNOWROB_VERSION = os.getenv('KNOWROB_VERSION')
@@ -41,15 +105,12 @@ query = api.model('Query', {
 
 # Endpoint
 ns = api.namespace('knowrob/api/' + KNOWROB_VERSION,
-                description='Operations related to KnowRob')
+                   description='Operations related to KnowRob')
 
-# ROS Client for prolog
-rosrest = RosprologRestClient()
 
-# Query interface implementation
 @ns.route("/query")
 class Query(Resource):
-    @ns.expect(query) # input model
+    @ns.expect(query)  # input model
     @ns.marshal_with(query)
     def post(self):
         rosrest.post_query(api.payload['query'])
@@ -57,8 +118,12 @@ class Query(Resource):
             api.payload['maxSolutionCount'])
         return api.payload
 
+
 if __name__ == '__main__':
     rospy.init_node('rosprolog_rest', anonymous=True)
-    
+
+    # ROS Client for prolog
+    rosrest = RosprologRestClient()
+
     http_server = WSGIServer(('', KNOWROB_PORT), app)
     http_server.serve_forever()
